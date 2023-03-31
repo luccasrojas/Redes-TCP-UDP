a14781b34ce7c1044f1a547e92b6e87e8041aaf4
Tony Montes <t.montes@uniandes.edu.co>
2023-03-30 22:48:49 -0500

    add timeout UDP + last logs TCP & UDP

--- udp/Files/create.py
+++ udp/Files/create.py
@@ -1,4 +1,5 @@
-# Write n 100MB random text files to the File100 directory
+# 1.
+# Write 10 140MB random text files to the File100 directory
 
 import random
 import string
@@ -10,7 +11,7 @@ os.mkdir('File100')
 
 # Create the files
 n=10 # number of files
-size=10 # MB
+size=14 # MB
 for i in range(n):
     filename = 'File100/' + str(i) + '.txt'
     with open(filename, 'w') as f:
@@ -25,3 +26,15 @@ shutil.make_archive('File100', 'zip', 'File100')
 # Delete the folder
 shutil.rmtree('File100')
 
+# 2.
+# Write 1 250MB random text file to the file2.txt file
+
+# Create the file
+size=250 # MB
+filename = 'file2.txt'
+with open(filename, 'w') as f:
+    for j in range(size*1000000):
+        f.write(random.choice(string.ascii_letters))
+
+print('File file2.txt created')
+print('File size:', os.path.getsize(filename), 'bytes')
--- udp/udp_client.py
+++ udp/udp_client.py
@@ -18,41 +18,48 @@ RECEIVED_DIR = './ArchivosRecibidos'
 def recibir_archivo(num_cliente):
     # Crear socket para conectarse con el servidor
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#    try:
-    client_socket.connect((IP, PORT))
-    print(f"Conexión establecida con el servidor para el cliente {num_cliente}.")
-    # Enviar mensaje de confirmación de recepción
-    print("Mandando peticion para conectar")
-    client_socket.sendto(b"Listo para conectar", ADDR)
+    client_socket.settimeout(3)  # Establecer timeout de 3 segundos
     
-    # Abrir archivo para escritura        
-    counter = 0
-    t1 = time.time()
-    with open(os.path.join(RECEIVED_DIR, f"Cliente{num_cliente}.txt"), 'wb') as f:
-        while True:
-            data, addr = client_socket.recvfrom(SIZE)
-            if data == b"FIN":
-                break
-            f.write(data)
-            counter += 1
-    print(counter)
-    t2 = time.time()
+    try:
+        client_socket.connect((IP, PORT))
+        print(f"Conexión establecida con el servidor para el cliente {num_cliente}.")
+        # Enviar mensaje de confirmación de recepción
+        print("Mandando peticion para conectar")
+        client_socket.sendto(b"Listo para conectar", ADDR)
 
-    print(f"Archivo recibido para el cliente {num_cliente}.")
-#    except:
-#        print(f"Error al recibir el archivo para el cliente {num_cliente}.")
-#    finally:
+        # Abrir archivo para escritura        
+        counter = 0
+        t1 = time.time()
+        with open(os.path.join(RECEIVED_DIR, f"Cliente{num_cliente}.txt"), 'wb') as f:
+            while True:
+                try:
+                    data, addr = client_socket.recvfrom(SIZE)
+                except socket.timeout:
+                    print(f"Timeout al recibir datos del servidor para el cliente {num_cliente}.")
+                    break  # Salir del while True si se excede el timeout
 
-    log_file_name = time.strftime("%Y-%m-%d-%H-%M-%S-log.txt")
-    log_file_path = os.path.join("./LogClient", log_file_name)
-    file_size = os.path.getsize(os.path.join(RECEIVED_DIR, f"Cliente{num_cliente}.txt"))
-    tiempo = t2-t1
-    with open(log_file_path, "a") as f:
-        f.write(f"Cliente: {num_cliente}, NombreArchivo: Cliente{num_cliente}.txt, TamanioArchivo: {file_size}, " 
-                f"Tiempo: {tiempo}\n")
+                if data == b"FIN":
+                    break
+                f.write(data)
+                counter += 1
+        print(counter)
+        t2 = time.time()
 
-    # Cerrar socket
-    client_socket.close()
+        print(f"Archivo recibido para el cliente {num_cliente}.")
+    except:
+        print(f"Error al recibir el archivo para el cliente {num_cliente}.")
+    finally:
+
+        log_file_name = time.strftime("%Y-%m-%d-%H-%M-%S-log.txt")
+        log_file_path = os.path.join("./LogClient", log_file_name)
+        file_size = os.path.getsize(os.path.join(RECEIVED_DIR, f"Cliente{num_cliente}.txt"))
+        tiempo = t2-t1
+        with open(log_file_path, "a") as f:
+            f.write(f"Cliente: {num_cliente}, NombreArchivo: Cliente{num_cliente}.txt, TamanioArchivo: {file_size}, " 
+                    f"Tiempo: {tiempo}\n")
+
+        # Cerrar socket
+        client_socket.close()
 
 if __name__ == "__main__":
     num_clientes = int(input("Ingrese el número de clientes: "))
